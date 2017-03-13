909fb02603d50b02dc62a9c67d304a58753a3525
Jackie <jaxmiles@gmail.com>
2017-03-13 19:39:17 +0000

    editing printingbuffer

--- printingbuffer.js
+++ printingbuffer.js
@@ -6,17 +6,18 @@ function getAndPrintHTML () {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
-
-    https.get(requestOptions, function (response) {
-      var arr = [];
+    var output = '';
+    https.get(requestOptions, function(response){
       response.setEncoding('utf8');
 
-    response.on('data', function (data) {
-      arr.push(data.toString());
-      console.log(arr) + '\n';
-    })
+
+    response.on('data', function (data){
+      output += data + '\n';
+
   });
-
-
+    response.on('end', function() {
+      console.log(output);
+    });
+});
 }
 getAndPrintHTML();
