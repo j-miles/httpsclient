a10fdb93fcd819c2e278b101c7e125620da85333
Jackie <jaxmiles@gmail.com>
2017-03-13 18:37:02 +0000

    added getAndPrintHTMLChunks

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.on('data', function (data) {
+    console.log('Received data:' + data + '\n');
+    })
+  });
+}
+getAndPrintHTMLChunks();
